c52a58c5606ee150678a8789de53f205449a8c96
Dealigh <gastondanielrodriguez@hotmail.com>
2022-06-26 13:09:52 -0300

    Refactored login method

--- src/test/java/com/solvd/automation/BasicTravelTest.java
+++ src/test/java/com/solvd/automation/BasicTravelTest.java
@@ -1,13 +1,15 @@
 package com.solvd.automation;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
-import com.solvd.automation.service.ILogin;
+import com.solvd.automation.webgui.service.Login;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
-public class BasicTravelTest extends AbstractTest implements ILogin {
+public class BasicTravelTest extends AbstractTest {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    protected Login login = new Login();
 }
